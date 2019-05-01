2494ea13be85e9eaedc7203668cb397d2a96eb0d
root <kino19071982@gmail.com>
2019-05-01 12:06:46 +0100

    Changes in files that had references to tecktinkering web site.

--- redirect/index.php
+++ redirect/index.php
@@ -2,18 +2,15 @@
 
 // Array mapping article ids to new descriptive urls
 $articleID = array(
-  3 => "/2008/10/07/book-review-fundamentals-of-operating-systems-by-a-m-lister",
-  2 => "/2008/09/16/how-to-share-an-ssl-certificate-and-still-use-cookies",
-  1 => "/2008/08/08/is-cobol-really-understandable-after-14-years"
 );
 
 if (array_key_exists($_GET[id], $articleID)) {
 	// Redirect to new post page
-	header("Location: http://techtinkering.com/".$articleID[$_GET[id]]);
+	header("Location: http://dr-kino.github.io/".$articleID[$_GET[id]]);
 } elseif ($_GET[tag]) {
-  header("Location: http://techtinkering.com/tag/".$_GET[tag]);
+  header("Location: http://dr-kino.github.io/tag/".$_GET[tag]);
 } else {
-  header("Location: http://techtinkering.com/");
+  header("Location: http://dr-kino.github.io/");
 }
 exit;
 
